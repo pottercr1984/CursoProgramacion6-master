5ae0f189229aca41cccb80caebf0b950bd1a78aa
estebanqj84 <estebanqj1984@gmail.com>
2021-06-17 13:10:13 -0600

    update

--- WBL/MarcaVehiculoService.cs
+++ WBL/MarcaVehiculoService.cs
@@ -8,11 +8,17 @@ using System.Threading.Tasks;
 
 namespace WBL
 {
-    public class MarcaVehiculoService
+    public interface IMarcaVehiculoService
+    {
+        Task<IEnumerable<MarcaVehiculoEntity>> Get();
+        Task<MarcaVehiculoEntity> GetById(MarcaVehiculoEntity entity);
+    }
+
+    public class MarcaVehiculoService : IMarcaVehiculoService
     {
         private readonly IDataAccess sql;
 
-        public MarcaVehiculoService(IDataAccess _sql )
+        public MarcaVehiculoService(IDataAccess _sql)
         {
             sql = _sql;
         }
@@ -31,9 +37,9 @@ namespace WBL
 
                 throw;
             }
-        
-        
-        
+
+
+
         }
 
         public async Task<MarcaVehiculoEntity> GetById(MarcaVehiculoEntity entity)
@@ -52,12 +58,78 @@ namespace WBL
 
                 throw;
             }
-        
-        
+
+
+
+        }
+
+        public async Task<DBEntity> Create(MarcaVehiculoEntity entity)
+        {
+            try
+            {
+                var result = sql.ExecuteAsync("MarcaVehiculoInsertar", new
+                {
+                    entity.Descripcion,
+                    entity.Estado
+                });
+
+                return await result;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+
+        }
+
+        public async Task<DBEntity> Update(MarcaVehiculoEntity entity)
+        {
+            try
+            {
+                var result = sql.ExecuteAsync("MarcaVehiculoActualizar", new
+                {
+                    entity.MarcaVehiculoId,
+                    entity.Descripcion,
+                    entity.Estado
+                });
+
+                return await result;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+
         }
 
 
 
+        public async Task<DBEntity> Delete(MarcaVehiculoEntity entity)
+        {
+            try
+            {
+                var result = sql.ExecuteAsync("MarcaVehiculoEliminar", new
+                {
+                    entity.MarcaVehiculoId
+                   
+                });
+
+                return await result;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+
+        }
 
     }
+
+
 }
